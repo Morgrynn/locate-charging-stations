d8faf6761fb68068deeb000e2dfd2dac2d24c9dd
Anthony <t9haan01@students.oamk.fi>
2020-09-28 13:19:44 +0300

    Adding components Header, Login, Map

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <div className={styles.navbar}>
+      <nav>
+        <ul className={styles.list}>
+          <Link to='/' className={styles.active}>
+            Locate Charging Station
+          </Link>
+          <Link to='/login'>Login</Link>
+        </ul>
+      </nav>
+    </div>
+  );
+}
---
+++ client/src/components/Map.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Map() {
+  return <div>This is a map</div>;
+}
