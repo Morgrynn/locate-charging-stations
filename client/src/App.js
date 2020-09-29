dd9cd26073cc1e83fbae96a90a069a21f72e7a24
Anthony <t9haan01@students.oamk.fi>
2020-09-29 22:04:00 +0300

    Favicon update & Login setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Map from './components/Map';
+import Main from './components/Main';
 import Login from './components/Login';
 
 function App() {
@@ -10,12 +10,8 @@ function App() {
     <div className='App'>
       <Header />
       <Switch>
-        <Route exact path='/' component={Map}>
-          <Map />
-        </Route>
-        <Route exact path='/login' component={Login}>
-          <Login />
-        </Route>
+        <Route exact path='/' component={Main} />
+        <Route exact path='/login' component={Login} />
       </Switch>
     </div>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import styles from './Login.module.css';
 
+// User login system which authenticates and authorizes users to access the system
+// TODO
+// make this a modal
+// link to back end
 export default function Login() {
-  return <div>This is Login page</div>;
+  return (
+    <form>
+      <div className={styles.container}>
+        <div>
+          <label>Username</label>
+          <input type='text' placeholder='Enter Username' name='username' />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type='password' placeholder='Enter Password' name='password' />
+        </div>
+        <div>
+          <button type='submit'>Login</button>
+        </div>
+      </div>
+      {/* <div className='container'>
+        <button type='button' class='cancelbtn'>
+          Cancel
+        </button>
+        <span className='password'>
+          Forgot <a href='#'>password?</a>
+        </span>
+      </div> */}
+    </form>
+  );
 }
