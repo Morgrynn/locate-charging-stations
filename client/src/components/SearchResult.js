107489752a8db8b089daeedfc5d38c62cc08ae02
Anthony <t9haan01@students.oamk.fi>
2020-10-08 13:10:27 +0300

    Use renderprops from SearchResult to ToggleComponent

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './styles/SearchResult.module.css';
+import ToggleComponent from './ToggleComponent';
 
 export default function SearchResult(props) {
   let filteredLocations = props.locationDataSet.filter((station) => {
@@ -10,15 +11,50 @@ export default function SearchResult(props) {
     );
   });
 
-  let displayList = <div></div>;
+  let displayList = null;
 
-  if (props.searchLocation.length != 0) {
+  if (props.searchLocation.length !== 0) {
     return (displayList = (
       <>
         {filteredLocations.map((station, i) => {
+          {
+            /* console.log(station.AddressInfo.Town); */
+          }
           return (
-            <li key={i} className={styles.listItem}>
-              {station.AddressInfo.Title}
+            <li
+              key={i}
+              onClick={() => {
+                props.clickedLocation(
+                  station.AddressInfo.Latitude,
+                  station.AddressInfo.Longitude
+                );
+              }}
+              className={styles.listItem}>
+              <p>
+                <strong>Place Name:</strong> {station.AddressInfo.Title}
+              </p>
+              <p>
+                <strong>Town:</strong> {station.AddressInfo.Town}
+              </p>
+              <ToggleComponent>
+                {({ on, toggle }) => {
+                  return (
+                    <div>
+                      <button onClick={toggle}>More Inforation ...</button>
+                      {on && (
+                        <>
+                          <p>{station.AddressInfo.AccessComments}</p>
+                          <p>Cost: {station.UsageCost}</p>
+                          <p>Type: {station.UsageType.Title}</p>
+                          <button onClick={props.checkLogin}>
+                            Charge Vehicle
+                          </button>
+                        </>
+                      )}
+                    </div>
+                  );
+                }}
+              </ToggleComponent>
             </li>
           );
         })}
