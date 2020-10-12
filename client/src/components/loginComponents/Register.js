59c46216b48921852c8ccc058b2dfdcbcf65fcec
Anthony <t9haan01@students.oamk.fi>
2020-10-12 08:57:20 +0300

    Styling for Register page

--- client/src/components/loginComponents/Register.js
+++ client/src/components/loginComponents/Register.js
@@ -5,7 +5,6 @@ import styles from './Register.module.css';
 export default function Register(props) {
   return (
     <div className={styles.mainContainer}>
-      Registration<Link to='/login'>Back</Link>
       <div className={styles.responsive}>
         <div className={styles.container}>
           <div className={styles.textMono}>Join LocalCharge</div>
@@ -13,30 +12,41 @@ export default function Register(props) {
         </div>
         <div className={styles.formContainer}>
           <form className={styles.regForm} onSubmit={props.register}>
-            <input
-              className={styles.input}
-              type='text'
-              placeholder='Enter Username'
-              name='username'
-              onChange={props.getRegUsername}
-            />
-            <input
-              className={styles.input}
-              type='email'
-              placeholder='Enter Email'
-              name='email'
-              onChange={props.getRegEmail}
-            />
-            <input
-              className={styles.input}
-              type='password'
-              placeholder='Enter password'
-              name='password'
-              onChange={props.getRegPassword}
-            />
-            <button className={styles.button} type='submit'>
-              Register!
-            </button>
+            <div className={styles.div}>
+              <input
+                className={styles.input}
+                type='text'
+                placeholder='Enter Username'
+                name='username'
+                onChange={props.getRegUsername}
+              />
+            </div>
+            <div className={styles.div}>
+              <input
+                className={styles.input}
+                type='email'
+                placeholder='Enter Email'
+                name='email'
+                onChange={props.getRegEmail}
+              />
+            </div>
+            <div className={styles.div}>
+              <input
+                className={styles.input}
+                type='password'
+                placeholder='Enter password'
+                name='password'
+                onChange={props.getRegPassword}
+              />
+            </div>
+            <div className={styles.div}>
+              <div className={styles.buttonLink}>
+                <Link to='/users/login'>Back</Link>
+              </div>
+              <button className={styles.button} type='submit'>
+                Register!
+              </button>
+            </div>
           </form>
         </div>
       </div>
